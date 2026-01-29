cb8bc53b5221154569b092be569235478d0b9783
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-01-29 13:03:50 +0300

    feat: settings screen fallback design

--- src/routes/_app/settings/index.tsx
+++ src/routes/_app/settings/index.tsx
@@ -1,9 +1,20 @@
+import { Empty, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty'
 import { createFileRoute } from '@tanstack/react-router'
+import { InfoIcon } from 'lucide-react'
 
 export const Route = createFileRoute('/_app/settings/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/settings/"!</div>
+  return (
+    <Empty className="border border-dashed">
+      <EmptyHeader>
+        <EmptyMedia variant={'icon'}>
+          <InfoIcon/>
+        </EmptyMedia>
+        <EmptyTitle>Coming Soon.....</EmptyTitle>
+      </EmptyHeader>
+    </Empty>
+  )
 }
